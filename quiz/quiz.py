6e2a0b5bbcc9f05939e7dd44e711f2124dc197ba
MateuszRajmundKukawski <mrkukawski@gmail.com>
2015-07-06 19:15:38 +0200

    formularz - cd

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -4,6 +4,8 @@
 from flask import Flask
 from flask import render_template
 
+from flask import request, redirect, url_for, flash
+
 app = Flask(__name__)
 
 # konfiguracja aplikacji
@@ -32,10 +34,24 @@ PYTANIA = [
 
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    
+
+    if request.method == 'POST':
+        punkty = 0
+        odpowiedzi = request.form
+
+        for pnr, odp_u in odpowiedzi.items():
+            if odp_u == PYTANIA[int(pnr)]['odpok']:
+                punkty += 1
+
+        flash(u'Liczba poprawnych odpowiedzi, to: {0}'.format(punkty))
+        return redirect(url_for('index'))
+
+    #return 'Cześć, tu Python!'
     return render_template('index.html', pytania=PYTANIA)
+    #return 'Cześć, tu Python!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
