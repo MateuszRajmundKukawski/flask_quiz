554c4b81b674589da480d200b045fb57f8cbffb0
MateuszRajmundKukawski <mrkukawski@gmail.com>
2015-07-06 19:06:54 +0200

    formularz - pocztek

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,1 +1,41 @@
-__author__ = 'mati'
+# -*- coding: utf-8 -*-
+# quiz/quiz.py
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+# konfiguracja aplikacji
+app.config.update(dict(
+    SECRET_KEY='bradzosekretnawartosc',
+))
+
+# lista pytań, lista zawirający słowniki
+PYTANIA = [
+    {
+        'pytanie': u'Stolica Hiszpani, to:',# pytanie
+        'odpowiedzi': [u'Madryt', u'Warszawa', u'Barcelona'], # możliwe odpowiedzi
+        'odpok': u'Madryt', # poprawna odpowiedź
+    },
+    {
+        'pytanie': u'Objętość sześcianu o boku 6 cm, wynosi:',
+        'odpowiedzi': [u'36', u'216', u'18'],
+        'odpok': u'216',
+    },
+    {
+        'pytanie': u'Symbol pierwiastka Helu, to:',
+        'odpowiedzi': [u'Fe', u'H', u'He'],
+        'odpok': u'He',
+    }
+]
+
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    
+    return render_template('index.html', pytania=PYTANIA)
+
+if __name__ == '__main__':
+    app.run(debug=True)
